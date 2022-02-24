b7ccf74ec9eaa4f0f876b988674566eecac747a7
ruman <mohammedruman1996@gmail.com>
2022-02-24 15:31:56 +0530

    added one more program.

---
+++ day17java/src/day17java/CollectionsDemo3.java
@@ -0,0 +1,30 @@
+package day17java;
+
+import java.util.LinkedList;
+
+public class CollectionsDemo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+					LinkedList <Integer> oj = new LinkedList<>();
+				    oj.add(1000);
+				    oj.add(2000);
+				    oj.add(3000);
+				    oj.add(4000);
+				    oj.add(5000);
+				    oj.add(6000);
+				    oj.add(7000);
+				    System.out.println(oj);
+				    oj.remove();
+				    System.out.println(oj);
+				    oj.peek();
+				    System.out.println(oj);
+				    oj.set(0,10000);
+				    oj.set(4,10000);
+				    System.out.println(oj);
+				
+				}
+
+			}
+
